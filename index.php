20263feaa9a66f4857e2d856cf36d5600b820cdb
dgm <dgermanoprofissional@gmail.com>
2021-08-22 11:34:27 -0300

    inicial

---
+++ index.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Source+Serif+Pro:wght@300;400;600;700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="main.css">
+    <link rel="stylesheet" href="color-themes.css">
+    <title>Home</title>
+</head>
+<body>
+    
+</body>
+    <div id="main-container">
+        <div id="main-navbar" class="card full-x">
+            <h1>Hello</h1>
+            <p>Im the first section.</p>
+        </div>
+        <div class="card left-x add-stroke stk-mid">
+            <ul class="list">
+                <li><a href="#">Link</a></li>
+                <li><a href="#">Link</a></li>
+                <li><a href="#">Link</a></li>
+            </ul>
+        </div>
+        <div class="card midright-x">
+            <p>Some text goes here.</p>
+        </div>
+        <div class="card full-x">
+            <ul class="list hr">
+                <li><a href="#">Link</a></li>
+                <li><a href="#">Link</a></li>
+                <li><a href="#">Link</a></li>
+                <li><a href="#">Link</a></li>
+            </ul>
+        </div>
+        
+        <div class="card left-x black">
+            <p>Some text goes here.</p>
+        </div>
+        <div class="card midright-x dark">
+            <p>Some text goes here.</p>
+        </div>
+        <div class="card midleft-x middark">
+            <p>Some text goes here.</p>
+        </div>
+        <div class="card right-x midlight add-stroke stk-dark">
+            <p>Some text goes here.</p>
+        </div>
+        <div class="card mid-x light add-stroke stk-light">
+            <p>Some text goes here.</p>
+        </div>
+        <div class="card center-x white add-stroke stk-mid">
+            <p>Some text goes here.</p>
+        </div>
+    </div>
+</html>
